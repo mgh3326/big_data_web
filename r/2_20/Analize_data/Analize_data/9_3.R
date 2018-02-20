4b29420960f752f186676c77cf2e23b28e8c3410
mgh3326 <mgh3326@naver.com>
2018-02-20 15:18:56 +0900

    data analize update

--- r/2_20/Analize_data/Analize_data/9_3.R
+++ r/2_20/Analize_data/Analize_data/9_3.R
@@ -1,4 +1,23 @@
+library(foreign) # SPSS 파일 로드
+library(dplyr) # 전처리
+library(ggplot2) # 시각화
 
+library(readxl) # 엑셀 파일 불러오기
+setwd("c://easy_r")
+# 데이터 불러오기
+
+raw_welfare <- read.spss(file = "Koweps_hpc10_2015_beta1.sav", to.data.frame = T)
+# 복사본 만들기
+
+welfare <- raw_welfare
+welfare <- rename(welfare,
+                  sex = h10_g3, # 성별
+                  birth = h10_g4, # 태어난 연도
+                  marriage = h10_g10, # 혼인 상태
+                  religion = h10_g11, # 종교
+                  income = p1002_8aq1, # 월급
+                  code_job = h10_eco9, # 직종 코드
+                  code_region = h10_reg7) # 지역 코드
 #### 09-3 ####
 
 ## -------------------------------------------------------------------- ##
@@ -25,9 +44,9 @@ qplot(welfare$age)
 
 ## -------------------------------------------------------------------- ##
 age_income <- welfare %>%
-  filter(!is.na(income)) %>%
-  group_by(age) %>%
-  summarise(mean_income = mean(income))
+    filter(!is.na(income)) %>%
+    group_by(age) %>%
+    summarise(mean_income = mean(income))
 
 head(age_income)
 
